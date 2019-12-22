b886df73c3c630c6f1f8ba06879e80faaee89b4a
Mlipphardt <matthewlipphardt@gmail.com>
2019-12-22 03:38:56 -0500

    Added html jumbotron, buttons for javascript. Defined javascript functions and variables.

---
+++ assets/javascript/script.js
@@ -0,0 +1,15 @@
+var dropthedot = document.getElementById("dropthedot");
+var recordfloor = document.getElementById("recordfloor");
+var dropthedotbutton = document.getElementById("dropthedotbutton");
+var recordfloorbutton = document.getElementById("recordfloorbutton");
+
+
+function chartme(number, totalhrs){
+    console.log((number)/(60*totalhrs))
+};
+
+function findfloor(timing){
+    print(1/(timing*60))
+};
+
+confirm("Are you looking to ")
